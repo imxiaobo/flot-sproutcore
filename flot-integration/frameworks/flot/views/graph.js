c0f6ca93d37fb40635aa76d842e2ce87343b57f0
Pepe Barbe <dev@antropoide.net>
2010-05-25 11:42:59 -0500

    Reorg

--- flot-integration/frameworks/flot/views/graph.js
+++ flot-integration/frameworks/flot/views/graph.js
@@ -10,22 +10,17 @@
 
   @extends SC.View
 */
+
 sc_require('core.js');
 Flot.GraphView = SC.View.extend(
 /** @scope Flot.GraphView.prototype */ {
-
-  // TODO: Add your own code here.
 	
 	data: null ,
-	
 	options: null ,
-	
 	debugInConsole: true ,
-	
 	concatenatedProperties: ['options'] ,
-	
 	render: function(context, firstTime) {
-		sc_super() ;
+		sc_super();
 		if(this.get('layer') && this.get('isVisibleInWindow')) {
 			if((this.get('frame').width > 0) && (this.get('frame').height > 0)) {
 				if(this.get('data')) {
@@ -35,55 +30,54 @@ Flot.GraphView = SC.View.extend(
 					if (this.debugInConsole) console.log('render');
 				}
 			}
-		}				
-	} ,
-				
-	viewDidResize: function() {
-		sc_super() ;
-		this.setLayerNeedsUpdate() ;
-	} ,	
-	
+		}
+	},
 	plotDataDidChange: function() {
 		this.setLayerNeedsUpdate() ;
 		if (this.debugInConsole) console.log('data changed');
 	}.observes('.data','.data.[]'),
-	
 	plotOptionsDidChange: function() {
 		this.setLayerNeedsUpdate() ;
 		if (this.debugInConsole) console.log('options changed');	
 	}.observes('.options'),
-	
 	visibilityDidChange: function() {
 		if(this.get('isVisibleInWindow') && this.get('isVisible')) {
 			if (this.debugInConsole) console.log('visibility changed');
 			this.setLayerNeedsUpdate() ;
 		}		
 	}.observes('isVisibleInWindow','isVisible'),
-	
 	layerDidChange: function() {
 		if (this.debugInConsole) console.log('layerchanged');
 		this.setLayerNeedsUpdate() ;	
 	}.observes('layer'),
-
 	layoutDidChange: function() {
-		sc_super() ;
+		sc_super();
 		if (this.debugInConsole) console.log('layout changed');
 		this.setLayerNeedsUpdate() ;
-	} ,
-	
+	},
 	updateLayerLocationIfNeeded: function() {
 		sc_super() ;
 		if (this.debugInConsole) console.log('layer location update');
 		this.setLayerNeedsUpdate() ;
-	} ,
-	
+	},
 	setLayerNeedsUpdate: function() {
 		this.invokeOnce(function() {
 			this.set('layerNeedsUpdate', YES);
 			if (this.debugInConsole) console.log('need update') ;
-		}) ;
+		});
 		
+	},
+	viewDidResize: function() {
+		sc_super();
+		this.setLayerNeedsUpdate() ;
+		//if (this.debugInConsole) 
+		console.log('view did resize');
+	}.observes('layout'),
+	parentViewDidResize : function() {
+		sc_super();
+		this.setLayerNeedsUpdate();
+		//if (this.debugInConsole) 
+		console.log('parent did resize');
 	}
-	
 
 });
