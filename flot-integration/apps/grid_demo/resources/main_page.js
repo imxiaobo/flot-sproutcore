4802935d07339998ff46d72f438409cccebc3cad
Pepe Barbe <dev@antropoide.net>
2010-05-26 10:22:50 -0500

    Nicer layout, tested only in WebKit

--- flot-integration/apps/grid_demo/resources/main_page.js
+++ flot-integration/apps/grid_demo/resources/main_page.js
@@ -11,15 +11,41 @@ GridDemo.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
 	mainPane: SC.MainPane.design({
-		childViews: 'add graphGrid'.w(),
+		childViews: 'add backDrop'.w(),
 		add: SC.ButtonView.design({
 			layout: { top: 20, right: 40, height: 24, width: 100 },
 			title: 'AddData',
 			action: 'addRandomData',
 			target: 'GridDemo.graphController'
 		}),
+		
+		backDrop: SC.View.design({
+			classNames: 'backdrop rounded'.w(),
+			backgroundColor: "#dedede",
+			borderStyle: SC.BORDER_GRAY,
+			layout: {left: 40, top: 50, bottom: 50, right: 40, minWidth: 615},
+			childViews: 'widgetView'.w(),
+			widgetView: SC.ScrollView.design({
+				classNames: 'rounded'.w(),
+				backgroundColor: 'white',
+				layout: {top: 20, bottom: 20, left: 20, right: 20, minWidth: 300},
+				hasHorizontalScroller: NO,
+				contentView: SC.GridView.design({
+					layout: {left: 10, right: 10, top: 10, bottom: 10},
+					classNames: 'rounded'.w(),
+					rowHeight: 200,
+					columnWidth: 300,
+					contentBinding: 'GridDemo.graphController',
+					isSelectable: NO,
+					exampleView: GridDemo.GridFlot
+				})
+			})
+		})
+		
+		
+		/*
 		graphGrid: SC.ScrollView.design({
-			layout: { top: 100, right: 40, bottom: 100, left: 40 },
+			layout: { top: 100, right: 40, bottom: 100, left: 40, minWidth: 800 },
 			contentView: SC.GridView.design({
 				rowHeight: 200,
 				columnWidth: 300,
@@ -27,5 +53,6 @@ GridDemo.mainPage = SC.Page.design({
 				exampleView: GridDemo.GridFlot
 			})
 		})
+		*/
 	})
 });
